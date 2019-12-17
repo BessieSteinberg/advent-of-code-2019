1dd8758c7c681841aa7b281751d5e424deae6b31
Bessie Steinberg <5060285+BessieSteinberg@users.noreply.github.com>
2019-12-16 23:34:07 -0700

    tests working

--- day01/main.py
+++ day01/main.py
@@ -1,17 +1,28 @@
 from math import floor
 
-def module_fuel(mass):
+def get_module_fuel(mass):
 	# Returns (int) of required fuel for given mass of a module
 
 	return floor( mass / 3 ) - 2
 
-def total_fuel(masses):
+def get_module_fuel_recursive(mass):
+	# Returns the amount of fuel required to get this mass movin'
+	# AND the fuel required to fuel the fuel (negative fuel is ignored)
+
+	fuel_mass = get_module_fuel(mass)
+
+	if fuel_mass <= 0:
+		return 0
+
+	return fuel_mass + get_module_fuel_recursive(fuel_mass)
+	
+
+def get_total_fuel(masses, fuel_mass_func=get_module_fuel):
 	# Returns total fuel required for the given list of masses
-	# TODO: turn into list comprehension
 
 	fuel_sum = 0
 
 	for mass in masses:
-		fuel_sum += module_fuel(mass)
+		fuel_sum += fuel_mass_func(mass)
 
 	return fuel_sum
--- day01/test.py
+++ day01/test.py
@@ -1,4 +1,4 @@
-from main import module_fuel, total_fuel
+from main import get_module_fuel, get_total_fuel, get_module_fuel_recursive
 
 import unittest
 import pytest
@@ -10,11 +10,25 @@ import pytest
 	(100756, 33583)
 ])
 def test_required_fuel(mass, expected_fuel):
-	actual_fuel = module_fuel(mass)
+	actual_fuel = get_module_fuel(mass)
 	assert actual_fuel == expected_fuel
 
 
 def test_total_fuel():
 	masses = [12, 14, 1969, 100756]
-	assert total_fuel(masses) == 34241
+	assert get_total_fuel(masses) == 34241
 
+@pytest.mark.parametrize("mass, expected_fuel", [
+	(14, 2),
+	(1969, 966),
+	(100756, 50346)
+])
+def test_get_module_fuel_and_itself(mass, expected_fuel):
+	actual_fuel = get_module_fuel_recursive(mass)
+	assert actual_fuel == expected_fuel
+
+
+def get_total_fuel_recursive():
+	masses = [14, 1969, 100756]
+	actual_fuel = get_total_fuel(masses, get_module_fuel_recursive)
+	assert actual_fuel == 50346
