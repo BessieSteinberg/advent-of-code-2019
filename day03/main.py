00d3c0b4687f6074992330f93071e5cdb61f7ec6
Bessie Steinberg <5060285+BessieSteinberg@users.noreply.github.com>
2019-12-17 13:26:16 -0700

    not sure why it's not working :"

--- day03/main.py
+++ day03/main.py
@@ -1,4 +1,6 @@
-class Pos():
+from itertools import count
+
+class Pos:
 	def __init__(self):
 		self.x = 0
 		self.y = 0
@@ -17,7 +19,6 @@ class Pos():
 
 	def go_down(self):
 		self.y -= 1
-
 
 	def go(self, direction):
 
@@ -46,13 +47,17 @@ def route_generator(wire_path):
 			yield pos.value()
 
 
-def closest_crossing(wire_path1, wire_path2):
-	# Return manhattan distance between starting point and closests crossing
-
+def get_crossings(wire_path1, wire_path2):
+	# Return (set) of crossings
 	path1 = set(route_generator(wire_path1))
 	path2 = set(route_generator(wire_path2))
 
-	crossings = path1.intersection(path2)
+	return path1.intersection(path2)
+
+
+def closest_crossing(wire_path1, wire_path2):
+	# Return manhattan distance between starting point and closests crossing
+	crossings = get_crossings(wire_path1, wire_path2)
 
 	distances = [abs(c[0]) + abs(c[1]) for c in crossings]
 	distances.sort()
@@ -60,7 +65,36 @@ def closest_crossing(wire_path1, wire_path2):
 	return distances[0]
 
 
+def closest_crossing_in_steps(wire_path1, wire_path2):
+	# Return the total number of steps it takes to get wire 1 and wire 2 to their closest crossing
 
+	# for each given position, provide the number of steps to get there the first time
+	steps1 = {}
+	step_counter = count(1)
+
+	for pos in route_generator(wire_path1):
+		current_step = next(step_counter)
+
+		# if this pos has not been visited yet create an entry for it
+		if not steps1.get(pos):
+			steps1[pos] = current_step
+
+	steps2 = {}
+	step_counter = count(1)
+
+	for pos in route_generator(wire_path1):
+		current_step = next(step_counter)
+
+		# if this pos has not been visited yet create an entry for it
+		if not steps2.get(pos):
+			steps2[pos] = current_step
+
+	crossings = get_crossings(wire_path1, wire_path2)
+
+	steps_to_crossings = [steps1[crossing] + steps2[crossing] for crossing in crossings]
+	steps_to_crossings.sort()
+
+	return steps_to_crossings[0]
 
 
 	
--- day03/test.py
+++ day03/test.py
@@ -1,7 +1,7 @@
-from main import route_generator, closest_crossing
+from main import route_generator, closest_crossing, closest_crossing_in_steps
 
-import unittest
 import pytest
+
 
 @pytest.mark.parametrize("wire_path, expected_route", [
 	(
@@ -64,6 +64,7 @@ def test_route_generator(wire_path, expected_route):
 
 	assert actual_route == expected_route
 
+
 @pytest.mark.parametrize("wire_path1, wire_path2, distance", [
 	(
 		"R75,D30,R83,U83,L12,D49,R71,U7,L72",
@@ -78,3 +79,25 @@ def test_route_generator(wire_path, expected_route):
 ])
 def test_closets_crossing(wire_path1, wire_path2, distance):
 	assert closest_crossing(wire_path1, wire_path2) == distance
+
+
+@pytest.mark.parametrize("wire_path1, wire_path2, steps", [
+	# (
+	# 	"R8,U5,L5,D3",
+	# 	"U7,R6,D4,L4",
+	# 	30
+	# ),
+	(
+		"R75,D30,R83,U83,L12,D49,R71,U7,L72",
+		"U62,R66,U55,R34,D71,R55,D58,R83",
+		610
+	),
+	# (
+	# 	"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+	# 	"U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+	# 	410
+	# )
+])
+def test_closest_crossing_in_steps(wire_path1, wire_path2, steps):
+	assert closest_crossing_in_steps(wire_path1, wire_path2) == steps
+
