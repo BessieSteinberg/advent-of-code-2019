dfde38dd9842fb120ea248015d0b585d6e833046
Bessie Steinberg <5060285+BessieSteinberg@users.noreply.github.com>
2019-12-16 23:36:26 -0700

    completed day1 star 2

--- day01/star2.py
+++ day01/star2.py
@@ -1,10 +1,10 @@
-from main import get_total_fuel
+from main import get_total_fuel, get_module_fuel_recursive
 
 with open('input.txt', 'r') as f:
 	data = f.readlines()
 
 data = [int(d) for d in data]
 
-fuel_required = get_total_fuel(data)
+fuel_required = get_total_fuel(data, get_module_fuel_recursive)
 
 print(f"total fuel required: {fuel_required}")
